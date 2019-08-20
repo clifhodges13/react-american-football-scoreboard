c79fd851bfee88276bfefd0f2b242f6d89cf1898
Clif <clifhodges13@gmail.com>
2019-08-19 20:34:15 -0500

    working on some stretch. updating the layout a bit

--- src/App.js
+++ src/App.js
@@ -5,19 +5,19 @@ import BottomRow from "./BottomRow";
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
-  const [lionScore, setLionScore] = useState(0);
-  const [tigerScore, setTigerScore] = useState(0);
+  const [homeScore, setHomeScore] = useState(0);
+  const [awayScore, setAwayScore] = useState(0);
   const homeTouchdown = e => {
-    setLionScore(lionScore + 7)
+    setHomeScore(homeScore + 7)
   }
   const homeFieldGoal = e => {
-    setLionScore(lionScore + 3)
+    setHomeScore(homeScore + 3)
   }
   const awayTouchdown = e => {
-    setTigerScore(tigerScore + 7)
+    setAwayScore(awayScore + 7)
   }
   const awayFieldGoal = e => {
-    setTigerScore(tigerScore + 3)
+    setAwayScore(awayScore + 3)
   }
 
   return (
@@ -25,16 +25,16 @@ function App() {
       <section className="scoreboard">
         <div className="topRow">
           <div className="home">
-            <h2 className="home__name">Lions</h2>
+            <h2 className="home__name">Rams</h2>
 
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
 
-            <div className="home__score">{lionScore}</div>
+            <div className="home__score">{homeScore}</div>
           </div>
-          <div className="timer">00:03</div>
+          <div className="timer">11:03</div>
           <div className="away">
-            <h2 className="away__name">Tigers</h2>
-            <div className="away__score">{tigerScore}</div>
+            <h2 className="away__name">Saints</h2>
+            <div className="away__score">{awayScore}</div>
           </div>
         </div>
         <BottomRow />
